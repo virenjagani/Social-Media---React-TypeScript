6b0c7e51ff3756182773647ab8c0d97776ff1445
virenjagani <virenjagani0303@gmail.com>
2024-02-25 04:50:50 -0500

    register

--- Server/api/auth.js
+++ Server/api/auth.js
@@ -1,5 +1,37 @@
+const authModel = require("../models/authModel");
+const { validationResult, check } = require("express-validator");
+const bcrypt = require("bcryptjs");
+
 module.exports = {
   register: async (req, res) => {
-    return res.json({ msg: "done" });
+    try {
+      const userFound = await authModel.findOne({ email: req.body.email });
+      if (!userFound) {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        const user = await authModel.create({
+          ...req.body,
+          password: hashedPassword,
+        });
+        return res.status(200).json({
+          status: "success",
+          message: "User successfully created",
+          data: user,
+        });
+      } else {
+        return res.status(400).json({
+          status: "error",
+          message: "User already exist",
+          data: [],
+        });
+      }
+    } catch (error) {
+      return res.status(400).json({
+        status: "error",
+        message: "Something wrong",
+        data: [],
+      });
+    }
   },
 };
---
+++ Server/models/authModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const authSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  city: { type: String, required: true },
+  province: { type: String, required: true },
+});
+
+const authModel = mongoose.model("auth", authSchema);
+
+module.exports = authModel;
